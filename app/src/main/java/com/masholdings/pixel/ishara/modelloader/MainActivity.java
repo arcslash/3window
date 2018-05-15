acb55ddcb523f92e02ec8819bc4e6e5cda7933b2
amith01994 <amith01994@gmail.com>
2018-05-15 09:18:52 +0530

    Initial file commit

---
+++ app/src/main/java/com/masholdings/pixel/ishara/modelloader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.masholdings.pixel.ishara.modelloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
